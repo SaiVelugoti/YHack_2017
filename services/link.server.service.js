cc522fefceda0cf01fd600b54015c6632924bee5
palaparthi <palaparthy.sandeep@gmail.com>
2017-12-02 02:35:18 -0500

    first commit

--- services/link.server.service.js
+++ services/link.server.service.js
@@ -12,13 +12,12 @@ function getAccounts(req, res) {
      var t = req.query["public_token"];
     //console.log("Hello -> link server");
     //console.log(req.body);
-    plaidClient.exchangePublicToken(t, function(err, rsp1) {
-        console.log(rsp1);
-        const access_token = rsp1.access_token;
-        res.json(rsp1.access_token);
-        // plaidClient.getAccounts(access_token, function(err, rsp2) {
-        //     console.log(rsp2.accounts);
-        //     res.json(rsp2.accounts);
-        // });
+    plaidClient.exchangePublicToken(t).then (rsp1 => {
+        console.log(rsp1.access_token);
+        return res.json(rsp1.access_token);
+    }).catch(err => {
+        throw new Error(`Unreachable code block for example: ${err}`);
     });
+        
+
 }
